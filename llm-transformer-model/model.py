be22f2736c316db36ead87f60f10feffa2d43233
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2024-11-24 10:11:27 +0600

    chore: Add FeedForward and MultiHeadAttention modules to LLM Transformer Model

--- llm-transformer-model/model.py
+++ llm-transformer-model/model.py
@@ -50,3 +50,71 @@ class LayerNormalization(nn.Module):
         mean = x.mean(dim = -1, keepdim=True)
         std = x.std(dim = -1, keepdim=True)
         return self.alpa * (x - mean) / (std + self.eps) + self.beta
+
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float):
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) # w1 and b1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) # w2 and b2
+
+    def forward(self, x):
+        # x: (batch_size, seq_len, d_model)
+        x = self.dropout(torch.relu(self.linear_1(x)))
+        x = self.linear_2(x)
+        return x
+
+class MultiHeadAttention(nn.Module):
+    def ___init__(self, d_model: int, heads: int, dropout: float):
+        super().__init__()
+        self.d_model = d_model
+        self.heads = heads
+
+        assert d_model % heads == 0, "Embedding dimension must be divisible by number of heads"
+
+        self.d_k = d_model // heads
+
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+
+        self.w_o = nn.Linear(d_model, d_model)
+
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        # (batch_size, heads, seq_len, d_k)
+        attention__scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+
+        if mask is not None:
+            # (batch_size, heads, seq_len, seq_len)
+            attention__scores.masked_fill_(mask == 0, -1e9)
+
+        attention__scores = attention__scores.softmax(dim = -1) # (batch_size, heads, seq_len, seq_len)
+        if dropout is not None:
+            attention__scores = dropout(attention__scores)
+
+        return (attention__scores @ value), attention__scores
+    
+    def forward(self, q, k, v, mask):
+        # (batch_size, seq_len, d_model): (q, k, v)
+        query = self.w_q(q)
+        key = self.w_k(k)
+        value = self.w_v(v)
+
+        # (batch_size, seq_len, heads, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.heads, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.heads, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.heads, self.d_k).transpose(1, 2)
+
+        x, self.attension_scores = MultiHeadAttention.attention(query, key, value, mask, self.dropout)
+
+        # (batch_size, seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.heads * self.d_k)
+
+        # (batch_size, seq_len, d_model)
+        return self.w_o(x)
